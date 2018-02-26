7183db06f1e26a944115b2c13af29518977ce19a
viralpatel380 <viralpatel.patel78@gmail.com>
2018-02-26 16:10:34 +0530

    Firebase Addded YoBykes

--- app.js
+++ app.js
@@ -99,20 +99,84 @@
 
 					var jsonfile = require('jsonfile');
 					jsonfile.writeFile('jsonfile.json', result);
-					var name1 = result[0].age;
-					var name2 = result[0].Name;
+					for(var i=0; i<result.length; i++){
+						console.log(i);
+					
+					var chargerNov = result[i]["charger no"];
+					var chargerRatingv = result[i]["charger rating"];
+					var cityv = result[i]["city"];  
+					var colorv = result[i]["color"];
+					var controllerNov = result[i]["controller no"];
+					var converterNov = result[i]["converter no"];
+					var dealerCodev = result[i]["dealer code"];
+					var dealerNamev = result[i]["dealer name"];
+					var exciseInvoiceNumberv = result[i]["Excise Invoice no"];
+					var modelv = result[i]["model"];
+					var motorNov = result[i]["motor no"];
+					var ownerManualNov = result[i]["owner manual no"];
+					var serialNoBattery1v = result[i]["serial no btry 1"];
+					var serialNoBattery2v = result[i]["serial no btry 2"];
+					var serialNoBattery3v= result[i]["serial no btry 3"];
+					var serialNoBattery4v = result[i]["serial no btry 4"];
+					var serialNoBattery5v = result[i]["serial no btry 5"];
+					var serialNoBattery6v = result[i]["serial no btry 6"];
+					var serialNoBattery7v = result[i]["serial no btry 7"];
+					var serialNoBattery8v = result[i]["serial no btry 8"];
+					var soldTimeStampv = result[i]["time"];
+					var statev = result[i]["state"];
+					var statusv = result[i]["status"];
+					var typeVehiclev = result[i]["type vehicle"];
+					var vehicleChassisNoCodev = result[i]["chesis no"];
+					var vehicleInvoiceDatev = result[i]["invoice date"];
+					var vehicleInvoiceNov = result[i]["invoice no"];
+
+					
+
+					
+
+
+
+
+					//  console.log(name3);
+					//  console.log(name1);
+					//  console.log(name2);
 					
 				//	JSON.parse( JSON.stringify(name1 ) )
 			   		//var obj = JSON.parse(result[0]);
-				  	firebase.database().ref().child("Vehicle").push().set({
+					firebase.database().ref().child("Vehicle").push().set({
 					   
-					age : name1,
-					name : name2 
-					   
+						 chargerNo : chargerNov,
+						 chargerRating : chargerRatingv,
+						 city : cityv,
+						 color : colorv,
+						 controllerNo : controllerNov,
+						 converterNo : converterNov,
+						 dealerCode : dealerCodev,
+						 dealerName : dealerNamev,
+						 exciseInvoiceNumber : exciseInvoiceNumberv,
+						 model : modelv,
+						 motorNo : motorNov,
+						ownerManualNo : ownerManualNov,
+						 serialNoBattery1 : serialNoBattery1v,
+						 serialNoBattery2 : serialNoBattery2v,
+						 serialNoBattery3 : serialNoBattery3v,
+						 serialNoBattery4 : serialNoBattery4v,
+						 serialNoBattery5 : serialNoBattery5v,
+						 serialNoBattery6 : serialNoBattery6v,
+						serialNoBattery7 : serialNoBattery7v,
+						 serialNoBattery8 : serialNoBattery8v,
+						 soldTimeStamp : soldTimeStampv,
+						 state : statev,
+						 status : statusv,
+						typeVehicle : typeVehiclev,
+						vehicleChassisNoCode : vehicleChassisNoCodev,
+						 vehicleInvoiceDate : vehicleInvoiceDatev,
+						vehicleInvoiceNo :vehicleInvoiceNov
 				   });
 
-
-	            });
+				
+					}	});
+			
 	        } catch (e) {
 	            res.json({
 	                error_code: 1,
